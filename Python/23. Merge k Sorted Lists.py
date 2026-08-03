9d0d0ef9e742a5833adaf853f74a9e0c82e476dc
Wesley Pang <144632953+Wesley25Pang05@users.noreply.github.com>
2026-08-03 17:47:20 -0400

    Add mergeKLists function to merge k sorted lists
    Implement a solution to merge k sorted linked lists into one sorted linked list.

---
+++ Python/23. Merge k Sorted Lists.py
@@ -0,0 +1,22 @@
+#You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+#Merge all the linked-lists into one sorted linked-list and return it.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        arr = []
+        for node in lists:
+            while node:
+                arr.append(node.val)
+                node = node.next
+        arr.sort()
+        dummy = curr = ListNode()
+        for item in arr:
+            curr.next = ListNode(item)
+            curr = curr.next
+        return dummy.next
