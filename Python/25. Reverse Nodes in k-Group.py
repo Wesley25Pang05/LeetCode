1b560c13bfcdf71921fd2971c73f5e85d061092e
Wesley Pang <144632953+Wesley25Pang05@users.noreply.github.com>
2026-08-05 22:22:20 -0400

    Refactor comments and add analysis for k-group reversal
    Updated comments to improve clarity and added LeetCode analysis.

--- Python/25. Reverse Nodes in k-Group.py
+++ Python/25. Reverse Nodes in k-Group.py
@@ -1,6 +1,6 @@
-#Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
-#k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
-#You may not alter the values in the list's nodes, only nodes themselves may be changed.
+# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -28,3 +28,12 @@ class Solution:
             return self.reverseKGroup(prev, num - k)
 
         return prev
+
+# LeetCode Analysis:
+# Key Idea: Reverse linked list nodes in groups of k using pointer manipulation.
+# Current: Linked List / Recursion
+# Suggested: Linked List / Two Pointers
+# Current complexity: O(N)
+# Suggested complexity: O(N)
+# Readability: Excellent
+# Structure: Excellent
