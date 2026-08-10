37201ac6747c66fee43283169bb683b1f29acf3a
Wesley Pang <144632953+Wesley25Pang05@users.noreply.github.com>
2026-08-10 01:49:09 -0400

    Fix comment formatting in Merge k Sorted Lists

--- Python/23. Merge k Sorted Lists.py
+++ Python/23. Merge k Sorted Lists.py
@@ -1,5 +1,5 @@
-#You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-#Merge all the linked-lists into one sorted linked-list and return it.
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+# Merge all the linked-lists into one sorted linked-list and return it.
 
 # Definition for singly-linked list.
 # class ListNode:
